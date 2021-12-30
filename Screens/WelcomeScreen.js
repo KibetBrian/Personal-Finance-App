fd05285e9c97e042b725cc0493a588277d434c02
Kibet Brian <briankibet2040@gmail.com>
2021-12-31 01:02:01 +0300

    Adds welcome screen

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,12 +1,88 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View,Image, StyleSheet, TouchableHighlight, TouchableOpacity} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {configs} from '../config';
 
 const WelcomeScreen = () => {
+  const { colors } = configs;
+
+  const handleButtonPress = ()=>{
+    alert('Get Started Button Pressed');
+  }
+
+
+  function image(){
+    return (
+      <Image style={styles.image} source = {require("../assets/illustrations/get_started.jpg")}/>    )
+  }
+
+  function button (){
+    return (
+      <View>
+        <TouchableOpacity onPress={handleButtonPress} activeOpacity={0.7} style= {styles.buttonOpacity}>
+          <Text style= {styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
   return (
-    <View>
-      <Text>Welcome Screen</Text>
-    </View>
-  )
-}
+    <LinearGradient
+      style={{flex: 1}}
+      colors={[configs.colors.primary, configs.colors.secondary]}>
+      <View style={styles.parent}>
+
+        <View style={styles.top}>
+          {image()}
+        </View>
+
+        <View style={styles.bottom}>
+          <Text style= {styles.desc}>The only personal finance app you need to monitor you spending's</Text>
+          {button()}
+        </View>
+      </View>
+
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  parent: {
+    flex: 1,
+    padding: 30
+  },
+  top: {
+    flex: 1.4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  bottom: {
+    flex: 1,
+    alignContent:'center',
+    justifyContent: 'space-around'
+  },
+  desc:{
+    color: configs.colors.white,
+    fontSize: 22,
+    fontFamily: 'Mukta-Bold',
+    textAlign: 'center'
+  },
+  buttonOpacity:{
+    height: 50,
+    backgroundColor: configs.colors.white,
+    borderRadius: 15,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  buttonText:{
+    fontSize: 20,
+    color: configs.colors.primary,
+    fontFamily: 'Mukta-Bold'
+  },
+  image:{
+    width: '70%',
+    height: '70%',
+    borderRadius: 50
+  }
+});
 
 export default WelcomeScreen;
--- config.js
+++ config.js
@@ -2,5 +2,7 @@ export const configs = {
   colors: {
     primary: '#5CC074',
     secondary: '#4CA060',
+    white: "#ffffff",
+    black: "#000000"
   },
 };
